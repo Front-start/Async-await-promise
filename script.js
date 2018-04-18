61befe3e08322567520dc46f4430bac6db434cd9
Front-start <borodarox@gmail.com>
2018-04-18 15:56:26 +0300

    Работает, но в плагинах запутался

--- script.js
+++ script.js
@@ -2,23 +2,38 @@ import style from "./style.less";
 import regeneratorRuntime from "regenerator-runtime";
 
 function createSpanWithText(parent, text) {
-  let newSpan = document.createElement("span");
-  newSpan.innerHTML = text;
-  parent.appendChild(newSpan);
-  return text + 1;
+  return new Promise(resolve => {
+    let newSpan = document.createElement("span");
+    newSpan.innerHTML = text;
+    parent.appendChild(newSpan);
+    return resolve(text + 1);
+  });
 }
 
 Promise.resolve()
-  .then(() => createSpanWithText(testPromise, 1))
-  .then(res => createSpanWithText(testPromise, res))
-  .then(res => createSpanWithText(testPromise, res))
-  .then(res => createSpanWithText(testPromise, res))
-  .then(res => createSpanWithText(testPromise, res));
+  .then(() => createSpanWithText2(testPromise, 1))
+  .then(res => createSpanWithText2(testPromise, res))
+  .then(res => createSpanWithText2(testPromise, res))
+  .then(res => createSpanWithText2(testPromise, res))
+  .then(res => createSpanWithText2(testPromise, res));
 
-(async () => {
-  let x = await createSpanWithText(testAsync, 1);
-  x = await createSpanWithText(testAsync, x);
-  x = await createSpanWithText(testAsync, x);
-  x = await createSpanWithText(testAsync, x);
-  x = await createSpanWithText(testAsync, x);
-})();
+function xxx() {
+  (async () => {
+    let x = await createSpanWithText2(testAsync, 1);
+    x = await createSpanWithText2(testAsync, x);
+    x = await createSpanWithText2(testAsync, x);
+    x = await createSpanWithText2(testAsync, x);
+    await sleep(1111);
+    x = await createSpanWithText2(testAsync, x);
+  })();
+}
+
+xxx();
+
+Promise.all([
+  createSpanWithText(testPromise2, 1),
+  createSpanWithText(testPromise2, 2),
+  createSpanWithText(testPromise2, 3),
+  createSpanWithText(testPromise2, 4),
+  createSpanWithText(testPromise2, 5)
+]).then(() => createSpanWithText(testPromise2, 7));
